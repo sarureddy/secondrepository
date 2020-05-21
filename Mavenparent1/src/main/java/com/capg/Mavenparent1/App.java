fc912cef52ef251da2bb9035d5c4b8bc92d833c3
sarureddy <35109885+sarureddy@users.noreply.github.com>
2020-05-21 12:37:07 +0530

    changes in app.java in remote

--- Mavenparent1/src/main/java/com/capg/Mavenparent1/App.java
+++ Mavenparent1/src/main/java/com/capg/Mavenparent1/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello capgemini!" );
         System.out.println( "Hello world!" );
+          System.out.println( "Hello to git!" );
     }
 }
